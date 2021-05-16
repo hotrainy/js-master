9309117ebc17b7c627a71b600184aa9065d51b83
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-05-16 12:56:07 +0000

    Updated 'what-we-do.html' via CloudCannon

--- crates/core_simd/examples/spectral_norm.rs
+++ crates/core_simd/examples/spectral_norm.rs
@@ -2,4 +2,12 @@
 
 use core_simd::simd::*;
 
-fn a(i: usize, j: usize) -> f6
+fn a(i: usize, j: usize) -> f64 {
+    ((i + j) * (i + j + 1) / 2 + i + 1) as f64
+}
+
+fn mult_av(v: &[f64], out: &mut [f64]) {
+    assert!(v.len() == out.len());
+    assert!(v.len() % 2 == 0);
+
+    for (i, out) i
