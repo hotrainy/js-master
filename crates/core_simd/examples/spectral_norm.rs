cfdf00cc9e13aca18714595c01535b9bb4023808
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-05-16 19:01:12 +0000

    Miscellaneous fixes

--- crates/core_simd/examples/spectral_norm.rs
+++ crates/core_simd/examples/spectral_norm.rs
@@ -10,4 +10,10 @@ fn mult_av(v: &[f64], out: &mut [f64]) {
     assert!(v.len() == out.len());
     assert!(v.len() % 2 == 0);
 
-    for (i, out) i
+    for (i, out) in out.iter_mut().enumerate() {
+        let mut sum = f64x2::splat(0.0);
+
+        let mut j = 0;
+        while j < v.len() {
+            let b = f64x2::from_slice(&v[j..]);
+            let a = f64x2:
