b1263f52657f0e4ee25e51c0919359baf4d0b3f5
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-06-18 19:08:48 +0000

    Remove erroneous double-quotes around %c in Exec line

--- crates/core_simd/src/cast.rs
+++ crates/core_simd/src/cast.rs
@@ -3,4 +3,8 @@ use crate::simd::SimdElement;
 /// Supporting trait for `Simd::cast`.  Typically doesn't need to be used directly.
 ///
 /// # Safety
-/// Implementing this trait ass
+/// Implementing this trait asserts that the type is a valid vector element for the `simd_cast` or
+/// `simd_as` intrinsics.
+pub unsafe trait SimdCast: SimdElement {}
+
+// Safety: primitive number typ
