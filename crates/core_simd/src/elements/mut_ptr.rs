7ebd5adb0e864013d75607bd0f4df680f42171ba
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-07-31 13:46:46 +0000

    Update MobFoxSDK.podspec.json

--- crates/core_simd/src/elements/mut_ptr.rs
+++ crates/core_simd/src/elements/mut_ptr.rs
@@ -1,4 +1,9 @@
 use super::sealed::Sealed;
 use crate::simd::{intrinsics, LaneCount, Mask, Simd, SimdPartialEq, SupportedLaneCount};
 
-/// Operations on SIMD vectors of mutable 
+/// Operations on SIMD vectors of mutable pointers.
+pub trait SimdMutPtr: Copy + Sealed {
+    /// Vector of `usize` with the same number of lanes.
+    type Usize;
+
+    /// Vector of `isize` with the same numbe
