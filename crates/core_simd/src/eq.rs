24a38b4656ad598bcb111cf424880c77dc2a2611
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-08-31 16:29:28 +0000

    Added missing license header

--- crates/core_simd/src/eq.rs
+++ crates/core_simd/src/eq.rs
@@ -11,4 +11,10 @@ pub trait SimdPartialEq {
     #[must_use = "method returns a new mask and does not mutate the original value"]
     fn simd_eq(self, other: Self) -> Self::Mask;
 
-    /// Test if each lane is equal to the corresponding lane in 
+    /// Test if each lane is equal to the corresponding lane in `other`.
+    #[must_use = "method returns a new mask and does not mutate the original value"]
+    fn simd_ne(self, other: Self) -> Self::Mask;
+}
+
+macro_rules! impl_number {
+    { $($num
