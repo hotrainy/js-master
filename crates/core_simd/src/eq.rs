50a89b83e20071f9acc3d8a411b862f64afa6251
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-09-18 20:26:50 +0000

    2.1.25

--- crates/core_simd/src/eq.rs
+++ crates/core_simd/src/eq.rs
@@ -81,4 +81,10 @@ where
     type Mask = Mask<isize, LANES>;
 
     #[inline]
-    fn simd_
+    fn simd_eq(self, other: Self) -> Self::Mask {
+        self.addr().simd_eq(other.addr())
+    }
+
+    #[inline]
+    fn simd_ne(self, other: Self) -> Self::Mask {
+        self.ad
