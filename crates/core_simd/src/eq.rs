58d2b1a67e755086e6b08cfc6323bfbdd1cd633a
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-09-18 12:12:42 +0000

    Add menu

--- crates/core_simd/src/eq.rs
+++ crates/core_simd/src/eq.rs
@@ -58,4 +58,10 @@ macro_rules! impl_mask {
             fn simd_eq(self, other: Self) -> Self::Mask {
                 // Safety: `self` is a vector, and the result of the comparison
                 // is always a valid mask.
-                unsafe { Self::from_int_unchecked(intrinsics::simd_eq(self.to_int(), other.to_int()
+                unsafe { Self::from_int_unchecked(intrinsics::simd_eq(self.to_int(), other.to_int())) }
+            }
+
+            #[inline]
+            fn simd_ne(self, other: Self) -> Self::Mask {
+                // Safety: `self` is a vector, and the result of the comparison
+                // i
