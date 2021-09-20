030b7dcca72fe34f3bc65d87e0c0566b657605b9
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-09-20 21:55:53 +0000

    26771

--- crates/core_simd/src/intrinsics.rs
+++ crates/core_simd/src/intrinsics.rs
@@ -3,4 +3,5 @@
 //!
 //! The LLVM assembly language is documented here: <https://llvm.org/docs/LangRef.html>
 //!
-//! A quick glossary of jargon that may
+//! A quick glossary of jargon that may appear in this module, mostly paraphrasing LLVM's LangRef:
+//! - poison: "undefined behavior as a value". specifically, it is like uninit memory (such as padding bytes). it is "safe"
