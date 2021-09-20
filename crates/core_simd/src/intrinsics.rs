10a36269b191fa8c1e04c668cb100d6151836471
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-09-20 13:02:49 +0000

    [cleanup]

--- crates/core_simd/src/intrinsics.rs
+++ crates/core_simd/src/intrinsics.rs
@@ -1,1 +1,6 @@
-//! This module contains the LLVM intrinsics bindings that provi
+//! This module contains the LLVM intrinsics bindings that provide the functionality for this
+//! crate.
+//!
+//! The LLVM assembly language is documented here: <https://llvm.org/docs/LangRef.html>
+//!
+//! A quick glossary of jargon that may
