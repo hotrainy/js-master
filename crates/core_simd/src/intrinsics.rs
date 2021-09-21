39503dfc82e0e6a0a3364ba44eaa4abe9b8f070a
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-09-21 13:01:54 +0000

    Add `.npmignore`.

--- crates/core_simd/src/intrinsics.rs
+++ crates/core_simd/src/intrinsics.rs
@@ -6,4 +6,5 @@
 //! A quick glossary of jargon that may appear in this module, mostly paraphrasing LLVM's LangRef:
 //! - poison: "undefined behavior as a value". specifically, it is like uninit memory (such as padding bytes). it is "safe" to create poison, BUT
 //!   poison MUST NOT be observed from safe code, as operations on poison return poison, like NaN. unlike NaN, which has defined comparisons,
-//!   poison is neither true nor f
+//!   poison is neither true nor false, and LLVM may also convert it to undef (at which point it is both). so, it can't be conditioned on, either.
+//! - undef: "a value that is every value". functionally like poison, insofar as Rust is 
