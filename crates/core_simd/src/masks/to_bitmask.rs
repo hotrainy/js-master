3db29f8f9b9f963418f11abf04e7e35788950f7d
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-11-01 12:51:15 +0000

    bonding: modify only neigh_parms owned by us

--- crates/core_simd/src/masks/to_bitmask.rs
+++ crates/core_simd/src/masks/to_bitmask.rs
@@ -1,2 +1,11 @@
 use super::{mask_impl, Mask, MaskElement};
-us
+use crate::simd::{LaneCount, SupportedLaneCount};
+
+mod sealed {
+    pub trait Sealed {}
+}
+pub use sealed::Sealed;
+
+impl<T, const LANES: usize> Sealed for Mask<T, LANES>
+where
+    T: Ma
