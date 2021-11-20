53569a5bef97ff4c17902f28c1a75de9f490f858
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-11-20 05:58:54 +0000

    revert accidental commit

--- crates/core_simd/src/mod.rs
+++ crates/core_simd/src/mod.rs
@@ -10,3 +10,16 @@ mod alias;
 mod cast;
 mod elements;
 mod eq;
+mod fmt;
+mod iter;
+mod lane_count;
+mod masks;
+mod ops;
+mod ord;
+mod select;
+mod vector;
+mod vendor;
+
+#[doc = include_str!("core_simd_docs.md")]
+pub mod simd {
+    pub(crate) use crate::core_simd:
