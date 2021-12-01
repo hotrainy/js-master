ae5488977ab9927e3fa72c8866e995c9ff126921
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-12-01 21:38:30 +0000

    Version bump to 0.7.5

--- crates/core_simd/src/ord.rs
+++ crates/core_simd/src/ord.rs
@@ -2,4 +2,8 @@ use crate::simd::{
     intrinsics, LaneCount, Mask, Simd, SimdConstPtr, SimdMutPtr, SimdPartialEq, SupportedLaneCount,
 };
 
-/// Pa
+/// Parallel `PartialOrd`.
+pub trait SimdPartialOrd: SimdPartialEq {
+    /// Test if each lane is less than the corresponding lane in `other`.
+    #[must_use = "method returns a new mask and does not mutate the original value"]
+   
