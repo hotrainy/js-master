2a3dae70a87cd58d6996e5ec30e073127bc1037a
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-12-03 13:42:23 +0000

    app structure

--- crates/core_simd/src/ord.rs
+++ crates/core_simd/src/ord.rs
@@ -9,4 +9,8 @@ pub trait SimdPartialOrd: SimdPartialEq {
     fn simd_lt(self, other: Self) -> Self::Mask;
 
     /// Test if each lane is less than or equal to the corresponding lane in `other`.
-    #[must_use = "method returns a new mask 
+    #[must_use = "method returns a new mask and does not mutate the original value"]
+    fn simd_le(self, other: Self) -> Self::Mask;
+
+    /// Test if each lane is greater than the corresponding lane in `other`.
+    #[must_use = "method re
