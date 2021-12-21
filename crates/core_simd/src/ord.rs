7b7742d4a35a0c91599ddd6dc44896b2e845024b
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-12-21 12:31:40 +0000

    SAK-12938

--- crates/core_simd/src/ord.rs
+++ crates/core_simd/src/ord.rs
@@ -138,4 +138,8 @@ macro_rules! impl_float {
 
             #[inline]
             fn simd_ge(self, other: Self) -> Self::Mask {
-                // Safety: `self` is a vecto
+                // Safety: `self` is a vector, and the result of the comparison
+                // is always a valid mask.
+                unsafe { Mask::from_int_unchecked(intrinsics::simd_ge(self, other)) }
+            }
+    
