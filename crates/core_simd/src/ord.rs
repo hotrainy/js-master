965a01a16129fb87d74d717e70ba83e2cda2fa6c
hotrainy <113290152+hotrainy@users.noreply.github.com>
2021-12-29 18:35:45 +0000

    Update documentation/LibrariesLabs.md

--- crates/core_simd/src/ord.rs
+++ crates/core_simd/src/ord.rs
@@ -158,4 +158,6 @@ macro_rules! impl_mask {
         {
             #[inline]
             fn simd_lt(self, other: Self) -> Self::Mask {
-                // Safety: `sel
+                // Safety: `self` is a vector, and the result of the comparison
+                // is always a valid mask.
+                unsafe { Self::from_int_unchecked(intrinsics::simd_lt(self.to_int(), other.to_int())) }
