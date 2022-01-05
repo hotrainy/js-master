b2851a527a8934955da5aa0b7bedc90a9f4acd03
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-01-05 17:47:26 +0000

    Add more test

--- crates/core_simd/src/ord.rs
+++ crates/core_simd/src/ord.rs
@@ -245,4 +245,15 @@ where
 {
     #[inline]
     fn simd_max(self, other: Self) -> Self {
-        self.simd_lt(other).select(other, se
+        self.simd_lt(other).select(other, self)
+    }
+
+    #[inline]
+    fn simd_min(self, other: Self) -> Self {
+        self.simd_gt(other).select(other, self)
+    }
+
+    #[inline]
+    fn simd_clamp(self, min: Self, max: Self) -> Self {
+        assert!(
+  
