f949cfa5b576cfd6a2fb194f26ff79b5f7682745
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-01-17 15:59:07 +0000

    fixed lint errors

--- crates/core_simd/src/select.rs
+++ crates/core_simd/src/select.rs
@@ -1,4 +1,11 @@
 use crate::simd::intrinsics;
 use crate::simd::{LaneCount, Mask, MaskElement, Simd, SimdElement, SupportedLaneCount};
 
-impl<T, 
+impl<T, const LANES: usize> Mask<T, LANES>
+where
+    T: MaskElement,
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    /// Choose lanes from two vectors.
+    ///
+    /// For each lane in the mask,
