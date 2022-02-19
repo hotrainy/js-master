db7d38def8be15e1ac4ee64a6e61b63893f3b697
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-02-19 09:52:33 +0000

    Submodule Sync

--- crates/core_simd/src/swizzle.rs
+++ crates/core_simd/src/swizzle.rs
@@ -1,4 +1,9 @@
 use crate::simd::intrinsics;
 use crate::simd::{LaneCount, Simd, SimdElement, SupportedLaneCount};
 
-/// Constructs a new SIMD vector by copying elements from selected
+/// Constructs a new SIMD vector by copying elements from selected lanes in other vectors.
+///
+/// When swizzling one vector, lanes are selected by a `const` array of `usize`,
+/// like [`Swizzle`].
+///
+/// When swizzling two vectors, lanes are selected by 
