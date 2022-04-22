282e7c7e9575c020760ca80ff256229f30ed2f43
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-04-22 08:13:52 +0000

    0.1.5

--- crates/core_simd/src/swizzle.rs
+++ crates/core_simd/src/swizzle.rs
@@ -326,4 +326,8 @@ where
     /// ```
     /// # #![feature(portable_simd)]
     /// # use core::simd::Simd;
-    /// let a = Simd::fro
+    /// let a = Simd::from_array([0, 4, 1, 5]);
+    /// let b = Simd::from_array([2, 6, 3, 7]);
+    /// let (x, y) = a.deinterleave(b);
+    /// assert_eq!(x.to_array(), [0, 1, 2, 3]);
+    /// assert_eq!(y.to_array(), [4, 5,
