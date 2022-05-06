82732fac67383849b880752752bea63a44f0a58a
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-05-06 21:24:45 +0000

    md fix

--- crates/core_simd/src/to_bytes.rs
+++ crates/core_simd/src/to_bytes.rs
@@ -1,3 +1,7 @@
 macro_rules! impl_to_bytes {
     { $ty:ty, $size:literal } => {
-        impl<const LANES: usize> crate::simd::Simd<$ty
+        impl<const LANES: usize> crate::simd::Simd<$ty, LANES>
+        where
+            crate::simd::LaneCount<LANES>: crate::simd::SupportedLaneCount,
+            crate::simd::LaneCount<{{ $size * LANES }}>: crate::simd::SupportedLaneCount,
+ 
