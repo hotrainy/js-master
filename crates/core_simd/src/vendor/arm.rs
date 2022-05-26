307abe7bd32639bd2d1aab6aa9c5ef66b9b28f71
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-05-26 09:04:42 +0000

    increasing test coverage

--- crates/core_simd/src/vendor/arm.rs
+++ crates/core_simd/src/vendor/arm.rs
@@ -1,4 +1,12 @@
 #![allow(unused)]
 use crate::simd::*;
 
-#[cfg(target_a
+#[cfg(target_arch = "arm")]
+use core::arch::arm::*;
+
+#[cfg(target_arch = "aarch64")]
+use core::arch::aarch64::*;
+
+#[cfg(any(
+    target_arch = "aarch64",
+    all(target_arch = "arm", target_feature = "v7"
