4b9d0e0d7dd7409b4d0ce0be65c0e45490ac4157
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-05-26 18:39:50 +0000

    HID: logitech: read all 32 bits of report type bitfield

--- crates/core_simd/src/vendor/arm.rs
+++ crates/core_simd/src/vendor/arm.rs
@@ -9,4 +9,12 @@ use core::arch::aarch64::*;
 
 #[cfg(any(
     target_arch = "aarch64",
-    all(target_arch = "arm", target_feature = "v7"
+    all(target_arch = "arm", target_feature = "v7"),
+))]
+mod neon {
+    use super::*;
+
+    from_transmute! { unsafe f32x2 => float32x2_t }
+    from_transmute! { unsafe f32x4 => float32x4_t }
+
+    from_transmute! { unsafe u8
