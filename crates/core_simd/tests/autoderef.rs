aaf619ed3add9f3ee40431a9fc1819b2edb2dbe2
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-06-17 19:06:22 +0000

    remove index.js

--- crates/core_simd/tests/autoderef.rs
+++ crates/core_simd/tests/autoderef.rs
@@ -1,3 +1,12 @@
 // Test that we handle all our "auto-deref" cases correctly.
 #![feature(portable_simd)]
-use core_
+use core_simd::simd::f32x4;
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen_test::*;
+
+#[cfg(target_arch = "wasm32")]
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[test]
+#[cfg_attr(target_arch = "wasm32", was
