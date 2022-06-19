2bb12bd38b52ae06762e772896b301d2e0bbdede
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-06-19 17:33:35 +0000

    update brewfile

--- crates/core_simd/tests/autoderef.rs
+++ crates/core_simd/tests/autoderef.rs
@@ -9,4 +9,11 @@ use wasm_bindgen_test::*;
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[test]
-#[cfg_attr(target_arch = "wasm32", was
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn deref() {
+    let x = f32x4::splat(1.0);
+    let y = f32x4::splat(2.0);
+    let a = &x;
+    let b = &y;
+    assert_eq!(f32x4::splat(3.0), x + y);
+    assert_eq!(f32x4::splat(3.0), x
