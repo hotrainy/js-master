0d18c22fb9bcd953b63bebba941d3fbeeefbdff3
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-06-21 21:35:17 +0000

    binder: prevent kptr leak by using %pK format specifier

--- crates/core_simd/tests/mask_ops_impl/mask_macros.rs
+++ crates/core_simd/tests/mask_ops_impl/mask_macros.rs
@@ -1,2 +1,8 @@
 macro_rules! mask_tests {
-    { $vect
+    { $vector:ident, $lanes:literal } => {
+        #[cfg(test)]
+        mod $vector {
+            use core_simd::simd::$vector as Vector;
+            const LANES: usize = $lanes;
+
+            #[cfg(target_ar
