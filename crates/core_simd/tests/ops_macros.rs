e82c8ae084f5a9592bfe7361eb8a453892a653fd
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-09-01 17:50:40 +0000

    Random stuff

--- crates/core_simd/tests/ops_macros.rs
+++ crates/core_simd/tests/ops_macros.rs
@@ -1,3 +1,7 @@
 /// Implements a test on a unary operation using proptest.
 ///
-/// Compares the ve
+/// Compares the vector operation to the equivalent scalar operation.
+#[macro_export]
+macro_rules! impl_unary_op_test {
+    { $scalar:ty, $trait:ident :: $fn:ident, $scalar_fn:expr } => {
+  
