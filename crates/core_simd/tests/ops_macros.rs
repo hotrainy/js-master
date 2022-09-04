8747a464926e0e1ef543d4321768fe795e5a9ef5
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-09-04 08:56:21 +0000

    updated README.md file

--- crates/core_simd/tests/ops_macros.rs
+++ crates/core_simd/tests/ops_macros.rs
@@ -4,4 +4,7 @@
 #[macro_export]
 macro_rules! impl_unary_op_test {
     { $scalar:ty, $trait:ident :: $fn:ident, $scalar_fn:expr } => {
-  
+        test_helpers::test_lanes! {
+            fn $fn<const LANES: usize>() {
+                test_helpers::test_unary_elementwise(
+                    &<core_simd::simd::Simd<$scalar, LANES> 
