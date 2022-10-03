3857859ce0422623b0f415d4d48160811c18c9f8
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-10-03 07:58:24 +0000

    Update brushaloader.txt

--- crates/core_simd/tests/ops_macros.rs
+++ crates/core_simd/tests/ops_macros.rs
@@ -97,4 +97,9 @@ macro_rules! impl_common_integer_tests {
             fn reduce_sum<const LANES: usize>() {
                 test_helpers::test_1(&|x| {
                     test_helpers::prop_assert_biteq! (
-                        $vector::<LANES>::from_arra
+                        $vector::<LANES>::from_array(x).reduce_sum(),
+                        x.iter().copied().fold(0 as $scalar, $scalar::wrapping_add),
+                    );
+                    Ok(())
+                });
+         
