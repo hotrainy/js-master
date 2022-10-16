b0c41744f27283001dda4e48081a0ba12569d415
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-10-16 16:40:50 +0000

    Fix some issues.

--- crates/core_simd/tests/ops_macros.rs
+++ crates/core_simd/tests/ops_macros.rs
@@ -154,4 +154,7 @@ macro_rules! impl_common_integer_tests {
                 });
             }
 
-            fn redu
+            fn reduce_min<const LANES: usize>() {
+                test_helpers::test_1(&|x| {
+                    test_helpers::prop_assert_biteq! (
+                        $vector::<LANES
