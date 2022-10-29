7170c5ba0335329949eaaba433dbab1c1fc062c2
hotrainy <113290152+hotrainy@users.noreply.github.com>
2022-10-29 13:29:34 +0000

    Compile the assets

--- crates/core_simd/tests/ops_macros.rs
+++ crates/core_simd/tests/ops_macros.rs
@@ -211,4 +211,10 @@ macro_rules! impl_signed_tests {
                     )
                 }
 
-                fn div_min_may_overflow<const LANES: usize>
+                fn div_min_may_overflow<const LANES: usize>() {
+                    let a = Vector::<LANES>::splat(Scalar::MIN);
+                    let b = Vector::<LANES>::splat(-1);
+                    assert_eq!(a / b, a);
+                }
+
+ 
