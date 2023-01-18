43185a2fd16dcea99ebb7da01748ba64fc4e09c5
hotrainy <113290152+hotrainy@users.noreply.github.com>
2023-01-18 15:23:05 +0000

    UPDATES

--- crates/core_simd/tests/to_bytes.rs
+++ crates/core_simd/tests/to_bytes.rs
@@ -7,4 +7,8 @@ use core_simd::simd::Simd;
 #[test]
 fn byte_convert() {
     let int = Simd::<u32, 2>::from_array([0xdeadbeef, 0x8badf00d]);
-    let bytes = int.to_n
+    let bytes = int.to_ne_bytes();
+    assert_eq!(int[0].to_ne_bytes(), bytes[..4]);
+    assert_eq!(int[1].to_ne_bytes(), bytes[4..]);
+    assert_eq!(Simd::<u32, 2>::from_ne_bytes(bytes), int);
+}
