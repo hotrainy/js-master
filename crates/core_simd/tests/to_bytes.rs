039627236cb91fab0333aa20c781a2477a43aba8
hotrainy <113290152+hotrainy@users.noreply.github.com>
2023-01-18 12:25:29 +0000

    o making the groupId unique for each IT

--- crates/core_simd/tests/to_bytes.rs
+++ crates/core_simd/tests/to_bytes.rs
@@ -1,3 +1,10 @@
 #![feature(portable_simd, generic_const_exprs, adt_const_params)]
 #![allow(incomplete_features)]
-#![cfg
+#![cfg(feature = "generic_const_exprs")]
+
+use core_simd::simd::Simd;
+
+#[test]
+fn byte_convert() {
+    let int = Simd::<u32, 2>::from_array([0xdeadbeef, 0x8badf00d]);
+    let bytes = int.to_n
