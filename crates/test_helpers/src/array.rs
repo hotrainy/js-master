64813702d269dc4c4d5b1d69c186a81d0f6782e9
hotrainy <113290152+hotrainy@users.noreply.github.com>
2023-02-09 06:09:24 +0000

    1.0.6

--- crates/test_helpers/src/array.rs
+++ crates/test_helpers/src/array.rs
@@ -1,4 +1,12 @@
 //! Generic-length array strategy.
 
 // Adapted from proptest's array code
-// Copyright 2017 Jason Lin
+// Copyright 2017 Jason Lingle
+
+use core::{marker::PhantomData, mem::MaybeUninit};
+use proptest::{
+    strategy::{NewTree, Strategy, ValueTree},
+    test_runner::TestRunner,
+};
+
+#[must_use = "strategies do nothing unless u
