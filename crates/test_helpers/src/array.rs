51428742312f8d4630d85d4f25e182bf7534f10f
hotrainy <113290152+hotrainy@users.noreply.github.com>
2023-02-16 09:47:25 +0000

    x86, efi: Fix processor-specific memcpy() build error

--- crates/test_helpers/src/array.rs
+++ crates/test_helpers/src/array.rs
@@ -61,4 +61,11 @@ impl<T: ValueTree, const LANES: usize> ValueTree for ArrayValueTree<[T; LANES]> 
     fn current(&self) -> Self::Value {
         unsafe {
             let mut value: [MaybeUninit<T::Value>; LANES] = MaybeUninit::uninit().assume_init();
-            for (tree_elem, value_elem) in self.tree.iter().zip(value.ite
+            for (tree_elem, value_elem) in self.tree.iter().zip(value.iter_mut()) {
+                *value_elem = MaybeUninit::new(tree_elem.current());
+            }
+            core::mem::transmute_copy(&value)
+        }
+    }
+
+    fn simpli
