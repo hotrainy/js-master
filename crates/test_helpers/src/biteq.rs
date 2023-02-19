0e471a67e907922f0e458fc28fcef1bd4d2e8354
hotrainy <113290152+hotrainy@users.noreply.github.com>
2023-02-19 08:33:55 +0000

    USB: visor: fix null-deref at probe

--- crates/test_helpers/src/biteq.rs
+++ crates/test_helpers/src/biteq.rs
@@ -1,4 +1,13 @@
 //! Compare numeric types by exact bit value.
 
 pub trait BitEq {
-    fn biteq(&self, other
+    fn biteq(&self, other: &Self) -> bool;
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result;
+}
+
+impl BitEq for bool {
+    fn biteq(&self, other: &Self) -> bool {
+        self == other
+    }
+
+    fn fmt(&self,
