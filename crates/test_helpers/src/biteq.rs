f2e2c1e5187b919d4bdd4024884f9beb64a0131e
hotrainy <113290152+hotrainy@users.noreply.github.com>
2023-02-20 11:08:09 +0000

    asdfasdf

--- crates/test_helpers/src/biteq.rs
+++ crates/test_helpers/src/biteq.rs
@@ -24,4 +24,13 @@ macro_rules! impl_integer_biteq {
             }
 
             fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-           
+                write!(f, "{:?} ({:x})", self, self)
+            }
+        }
+        )*
+    };
+}
+
+impl_integer_biteq! { u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize }
+
+macro_r
